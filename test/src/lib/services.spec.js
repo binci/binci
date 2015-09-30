39cd0e38c9a6448b1da998345cdbb9ae040697bf
fluidbyte <kent@fluidbyte.net>
2015-09-30 13:40:27 -0400

    Adds tests for 'startSvc' method

--- test/src/lib/services.spec.js
+++ test/src/lib/services.spec.js
@@ -1,6 +1,7 @@
 /* global sinon, expect, request, describe, it, before, after */
 import './../../setup';
 import services from './../../../src/lib/services';
+import { exec } from 'child_process';
 
 describe('services', () => {
   describe('getObj', () => {
@@ -15,6 +16,34 @@ describe('services', () => {
         name: 'svcImage',
         image: 'svcImage'
       });
+    });
+  });
+  describe('startSvc', () => {
+    after((done) => {
+      exec('docker stop mongotest && docker rm mongotest', (err) => {
+        if (!err) {
+          done();
+        }
+      });
+    });
+    it('starts a service based on the object passed', (done) => {
+      services.startSvc({ name: 'mongotest', image: 'mongo' })
+        .then(done)
+        .catch(done);
+    });
+    it('resolves if service is already started', (done) => {
+      services.startSvc({ name: 'mongotest', image: 'mongo' })
+        .then(done)
+        .catch(done);
+    });
+    it('fails if invalid service is passed', (done) => {
+      services.startSvc({ name: 'noimage', image: 'noimage12345' })
+        .then(() => {
+          done('Should have failed');
+        })
+        .catch(() => {
+          done();
+        });
     });
   });
 });
