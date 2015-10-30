35b844b50776d028c909e80e52c98e11b979f3b6
TomFrost <tom@frosteddesign.com>
2015-10-30 10:16:38 -0400

    Tunnels: Remove unused imports

--- src/lib/tunnels.js
+++ src/lib/tunnels.js
@@ -1,11 +1,7 @@
 /*
  * Copyright (c) 2015 TechnologyAdvice
  */
-import proc from './process';
 import { spawn } from 'child_process';
-import Promise from 'bluebird';
-import output from './output';
-import parsers from './parsers';
 
 const bindIp = '127.0.0.1';
 
