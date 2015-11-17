04968772cdad125d2eec4bde2ca63902bb37c2a1
fluidbyte <kent@fluidbyte.net>
2015-11-17 13:34:46 -0500

    Do not use unique instance on persisted service names

--- src/lib/parsers.js
+++ src/lib/parsers.js
@@ -52,9 +52,10 @@ const parsers = {
    * @param {String} name
    * @returns {String}
    */
-  parseSvcObjName: (name) => {
+  parseSvcObjName: (name, persist) => {
     let user = username.sync() || 'unknown';
-    return `devlab_${name}_${user}_${config.instance}`.toLowerCase().replace(/[^A-Z0-9]/ig, '_');
+    const uid = (!persist) ? `_${config.instance}` : '';
+    return `devlab_${name}_${user}${uid}`.toLowerCase().replace(/[^A-Z0-9]/ig, '_');
   },
   /**
    * Parses the service object and ensures all required props set
@@ -63,7 +64,7 @@ const parsers = {
    */
   parseSvcObj: (svc) => {
     const image = Object.keys(svc)[0];
-    const name = parsers.parseSvcObjName(svc[image].name || image);
+    const name = parsers.parseSvcObjName(svc[image].name || image, svc[image].persist);
     const alias = svc[image].name || image;
     const env = svc[image].env || false;
     const expose = svc[image].expose || false;
