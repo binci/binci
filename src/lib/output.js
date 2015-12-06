dd1847e1cd69ab26a3d9b572ec6c9a47433cf75a
Jordan Denison <jordan@denisonweb.com>
2015-12-05 22:18:24 -0600

    Refactor: output.js

--- src/lib/output.js
+++ src/lib/output.js
@@ -10,28 +10,28 @@ const output = {
    * Colorizes {{...}} delimited vars
    * @param {String} m Raw message
    */
-  renderVars: (m) => m.toString()
+  renderVars: m => m.toString()
     .replace(/\{\{([^}]+)\}\}/g, (i, match) => clc.cyan(match))
     .replace('set -e;', ''),
   /**
    * Output success message
    * @param {String} m Output message
    */
-  success: (m) => {
+  success: m => {
     if (!output.quiet) output.log(clc.green.bold('#> ') + clc.bold(output.renderVars(m)));
   },
   /**
    * Output warning message
    * @param {String} m Output message
    */
-  warn: (m) => {
+  warn: m => {
     if (!output.quiet)output.log(clc.yellow.bold('!> ') + clc.bold(output.renderVars(m)));
   },
   /**
    * Output error message
    * @param {String} m Output message
    */
-  error: (m) => {
+  error: m => {
     if (!output.quiet) output.log(clc.red.bold('!> ') + clc.bold(output.renderVars(m)));
   },
   /**
@@ -50,7 +50,7 @@ const output = {
    * Standard log output, no formatting
    * @param {String} m Output message
    */
-  log: (m) => {
+  log: m => {
     if (!process.env.LAM_TEST) process.stdout.write(`${m}\n`);
   }
 };
