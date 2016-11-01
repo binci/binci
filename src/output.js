4c41b7ceb56171be2167ff52c01312c9f1d82673
fluidbyte <kent@fluidbyte.net>
2016-11-01 12:00:06 -0400

    doc(output): just some cleanup

--- src/output.js
+++ src/output.js
@@ -2,28 +2,30 @@
 const chalk = require('chalk')
 
 const output = {
-  // Holds quiet boolean
+  /**
+   * @property {boolean} enable/disable application messaging
+   */
   quiet: false,
   /**
    * Colorizes {{...}} delimited vars
-   * @param {String} m Raw message
+   * @param {string} m Raw message
    */
   renderVars: m => m.toString()
     .replace(/\{\{([^}]+)\}\}/g, (i, match) => chalk.blue(match))
     .replace('set -e;', ''),
   /**
    * Output success message
-   * @param {String} m Output message
+   * @param {string} m Output message
    */
   success: m => output.log(chalk.bold.green('⦿ ') + chalk.bold(output.renderVars(m))),
   /**
    * Output warning message
-   * @param {String} m Output message
+   * @param {string} m Output message
    */
   warn: m => output.log(chalk.bold.yellow('⦿ ') + chalk.bold(output.renderVars(m))),
   /**
    * Output error message
-   * @param {String} m Output message
+   * @param {string} m Output message
    */
   error: m => output.log(chalk.bold.red('⦿ ') + chalk.bold(output.renderVars(m))),
   /**
@@ -32,7 +34,7 @@ const output = {
   insertBreak: () => output.log(chalk.gray('---')),
   /**
    * Standard output
-   * @param {String} m Output message
+   * @param {string} m Output message
    */
   log: m => {
     /* istanbul ignore next */
