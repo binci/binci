8968dcebaaa4811bc3e191691c2a862d85dd8168
fluidbyte <kent@fluidbyte.net>
2016-11-02 09:50:46 -0400

    refactor(cleanup): removing old files, misc cleanup

--- test/project/node_modules/mongodb/test_boot/boot.sh
+++
@@ -1,3 +0,0 @@
-mongod --verbose --smallfiles --noprealloc --setParameter enableTestCommands=1 \
---port 39000 --dbpath ./data --sslMode allowSSL --sslCAFile ./ca.pem \
---sslPEMKeyFile ./server_password.pem --sslPEMKeyPassword serverpassword --nojournal
