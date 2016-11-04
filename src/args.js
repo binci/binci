5a63ba020e9a33f3c042bb1c19a598a988514f94
fluidbyte <kent@fluidbyte.net>
2016-11-03 20:52:01 -0400

    docs(args): add more jsdoc to lib

--- src/args.js
+++ src/args.js
@@ -7,6 +7,8 @@ const output = require('./output')
 const args = {
   /**
    * @property {object} available run-time flags
+   * - @property {string} action - the method to call to parse flag
+   * - @property {string} help - the help text to display to the user
    */
   available: {
     h: { action: 'showHelp', help: 'Displays help and usage' },
@@ -36,23 +38,32 @@ const args = {
   },
   /**
    * Sets the execution task
+   * @param {string} e the exec command
+   * @returns {object}
    */
   setExecCommand: (e) => new Object({ exec: e }),
   /**
    * Sets the primary container source (FROM)
+   * @param {string} f the container specification
+   * @returns {object}
    */
   setPrimaryFrom: (f) => new Object({ from: f }),
   /**
    * Sets custom config path
+   * @param {string} c the path to the config file
+   * @returns {object}
    */
   setCustomConf: (c) => new Object({ configPath: c }),
   /**
    * Sets quiteMode boolean
+   * @param {boolean} q whether or not quiet mode was passed
+   * @returns {object}
    */
   setQuietMode: (q) => new Object({ quietMode: true }),
   /**
    * Parse arguments
-   * @param {object} argObj Arguments
+   * @param {object} argObj arguments from invocation
+   * @returns {object}
    */
   parse: (argObj) => {
     const argsOut = {}
