ad700f026295a6e4706603fd9e9ca12f869027ca
Levi Thomason <me@levithomason.com>
2017-03-31 12:43:53 -0700

    feat(config): better config error messages

--- test/src/config.spec.js
+++ test/src/config.spec.js
@@ -5,16 +5,27 @@ const fs = Promise.promisifyAll(require('fs'))
 describe('config', () => {
   describe('load', () => {
     const confPath = `${process.cwd()}/devlab.yml`
-    before(() => fs.writeFileAsync(confPath, 'from: node:4'))
-    after(() => fs.unlinkAsync(confPath))
+    beforeEach(() => fs.writeFileAsync(confPath, 'from: node:4'))
+    afterEach(() => fs.unlinkAsync(confPath))
     it('loads the $CWD/devlab.yml default config location', () => {
       expect(config.load()).to.deep.equal({ from: 'node:4' })
     })
     it('loads the devlab.yml from a custom path when specified', () => {
       expect(config.load(confPath)).to.deep.equal({ from: 'node:4' })
     })
-    it('exits the process if file cannot be loaded', () => {
-      expect(() => config.load('/no/conf')).to.throw('Cannot load config file. Please ensure you have a valid ./devlab.yml file or specify one with the `-c` flag')
+    it('exits the process if file cannot be found', () => {
+      expect(() => config.load('/no/conf'))
+        .to.throw('Please create a ./devlab.yml file or specify one with the `-c` flag.')
+    })
+    it('exits the process if file cannot be parsed', () => {
+      const error = [
+        'Please fix the errors in test/fixtures/devlab-syntax-error.yml:',
+        '',
+        'can not read an implicit mapping pair; a colon is missed at line 13, column 29:',
+        '      echo "ruh roh! YLM error!"',
+        '                                ^'
+      ].join('\n')
+      expect(() => config.load('test/fixtures/devlab-syntax-error.yml')).to.throw(error)
     })
   })
 })
