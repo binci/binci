cac026909a755041c991695d870f1fda8eb74022
Peter Svetlichny <peter.svetlichny@technologyadvice.com>
2017-05-16 09:20:47 -0700

    test(utils): add checkVersion tests

--- test/src/utils.spec.js
+++ test/src/utils.spec.js
@@ -1,18 +1,21 @@
 const utils = require('src/utils')
-const fixture = require('test/fixtures/docker-ps')
 const sandbox = require('test/sandbox')
 const proc = require('src/proc')
 const output = require('src/output')
 const cp = require('child_process')
+const fixtures = {
+  ps: require('test/fixtures/docker-ps'),
+  v: require('test/fixtures/docker-v')
+}
 
-const fixtureIds = fixture.ids.split('\n').filter(Boolean)
-const fixtureAllIds = fixture.allIds.split('\n').filter(Boolean)
+const fixtureIds = fixtures.ps.ids.split('\n').filter(Boolean)
+const fixtureAllIds = fixtures.ps.allIds.split('\n').filter(Boolean)
 
 describe('utils', () => {
   describe('cleanup', () => {
     beforeEach(() => {
       sandbox.stub(cp, 'execSync', cmd => {
-        return Buffer.from(cmd.includes('--filter="name=dl_"') ? fixture.ids : fixture.allIds)
+        return Buffer.from(cmd.includes('--filter="name=dl_"') ? fixtures.ps.ids : fixtures.ps.allIds)
       })
       sandbox.stub(output, 'info')
       sandbox.stub(output, 'error')
@@ -66,7 +69,7 @@ describe('utils', () => {
   })
   describe('parseOrphans', () => {
     it('returns array of orphaned containers', () => {
-      expect(utils.parseOrphans(fixture.full)).to.deep.equal(['dl_orphan3_JKLod93dS', 'dl_orphan4_MNJ9ie00d'])
+      expect(utils.parseOrphans(fixtures.ps.full)).to.deep.equal(['dl_orphan3_JKLod93dS', 'dl_orphan4_MNJ9ie00d'])
     })
   })
   describe('checkOrphans', () => {
@@ -80,11 +83,23 @@ describe('utils', () => {
       })
     })
     it('resolves after warning of identified orphans', () => {
-      sandbox.stub(proc, 'exec', () => Promise.resolve(fixture.full))
+      sandbox.stub(proc, 'exec', () => Promise.resolve(fixtures.ps.full))
       return utils.checkOrphans().then(() => {
         expect(output.warn).to.be.calledWith(
           'These containers may not have exited correctly: dl_orphan3_JKLod93dS, dl_orphan4_MNJ9ie00d')
       })
+    })
+  })
+  describe('checkVersion', () => {
+    it('checks Docker version and sets global rmOnShutdown flag', () => {
+      sandbox.stub(proc, 'exec', () => Promise.resolve(fixtures.v.current))
+      return utils.checkVersion().then(() => {
+        expect(global.rmOnShutdown).to.be.false()
+      })
+    })
+    it('checks version and throws error if Docker not installed', () => {
+      sandbox.stub(proc, 'exec', () => Promise.reject(new Error()))
+      return expect(utils.checkVersion()).to.be.rejectedWith(Error, /Docker not installed/)
     })
   })
 })
