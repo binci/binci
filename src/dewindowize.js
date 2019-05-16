6b8c249eb5eb65c030568919f1dcb730f78d6811
Svet Nikolov <svnikolov@gmail.com>
2019-05-15 18:36:43 -0700

    fix(dewindowize): Removed debug statements

--- src/dewindowize.js
+++ src/dewindowize.js
@@ -7,10 +7,6 @@ const process = require('process')
  * @returns {String} the converted path
  */
 module.exports = (winPath) => {
-  console.log(process.platform)
-  console.log(path.isAbsolute(winPath))
-  console.log(winPath)
-
   if (process.platform === 'win32' && path.isAbsolute(winPath)) {
     return ('/' + winPath.replace(':', '').replace(new RegExp('\\\\', 'g'), '/'))
   } else {
