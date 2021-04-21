f16852c47e244f8cb843a018e1b489accad6f5c6
fluidbyte <kent@fluidbyte.net>
2021-04-21 20:24:43 +0000

    fix(lint): ignore ternanry

--- src/services.js
+++ src/services.js
@@ -1,3 +1,5 @@
+/* eslint no-unneeded-ternary: 0 */
+
 'use strict'
 
 const _ = require('halcyon')
@@ -23,28 +25,34 @@ const services = {
     const tasks = _.values(_.pick(cfg.run, cfg.tasks))
     const objs = _.filter(_.isType('object'), tasks)
     // If any running task doesn't have object config and no services specified in command line, keep all services
-    if (objs.length !== tasks.length && !services.disabled.length && !services.disableAll) return cfg
+    if (
+      objs.length !== tasks.length &&
+      !services.disabled.length &&
+      !services.disableAll
+    ) {
+      return cfg
+    }
     const allSvcs = _.chain(_.keys, cfg.services)
     let svcs
     if (services.disableAll) {
       svcs = allSvcs
     } else {
-      svcs = _.chain(t => t.disable === '*' ? allSvcs : t.disable, objs)
-      services.disabled.forEach(s => {
+      svcs = _.chain((t) => (t.disable === '*' ? allSvcs : t.disable), objs)
+      services.disabled.forEach((s) => {
         if (!_.contains(s, svcs)) svcs.push(s)
       })
     }
     // Track which services are disabled by running tasks
-    const counts = _.pipe([
-      _.groupBy(_.identity),
-      _.map(_.length)
-    ])(svcs)
+    const counts = _.pipe([_.groupBy(_.identity), _.map(_.length)])(svcs)
     // Add service to list if disabled by all running tasks
     services.disabled = _.keys(_.filter(_.equals(tasks.length), counts))
     /* istanbul ignore if: lots of work, testing doesn't prove anything... */
     if (!services.disabled.length) return cfg
     // Keep service if name is not in disabled list
-    cfg.services = _.filter((s) => !_.contains(_.head(_.keys(s)), services.disabled), cfg.services)
+    cfg.services = _.filter(
+      (s) => !_.contains(_.head(_.keys(s)), services.disabled),
+      cfg.services
+    )
     return cfg
   },
   /**
@@ -63,15 +71,29 @@ const services = {
    * @param {Object} cfg Instance config object
    * @returns {array} Array of services names, persistence and run args
    */
-  get: (cfg) => !cfg.services ? false : _.map(_.pipe([
-    _.toPairs,
-    _.head,
-    ([name, value]) => ({
-      name,
-      persist: value.persist || false,
-      stopTimeSecs: services.getStopTimeSecs(cfg, value),
-      args: command.get(_.merge(value, { rmOnShutdown: cfg.rmOnShutdown, privileged: cfg.privileged === false ? false : true }), name, null)
-    })]), cfg.services),
+  get: (cfg) =>
+    !cfg.services
+      ? false
+      : _.map(
+          _.pipe([
+            _.toPairs,
+            _.head,
+            ([name, value]) => ({
+              name,
+              persist: value.persist || false,
+              stopTimeSecs: services.getStopTimeSecs(cfg, value),
+              args: command.get(
+                _.merge(value, {
+                  rmOnShutdown: cfg.rmOnShutdown,
+                  privileged: cfg.privileged === false ? false : true
+                }),
+                name,
+                null
+              )
+            })
+          ]),
+          cfg.services
+        ),
   /**
    * Runs services and resolves or rejects
    * @param {array} svc Array of service command arrays
@@ -79,16 +101,23 @@ const services = {
    */
   run: (svc) => {
     const errors = []
-    return Promise.all(_.map((cur) => {
-      let curName = command.getName(cur.name, { persist: cur.persist })
-      return proc.exec(`docker ps -f name=${curName} -q`).then((res) => {
-        if (res && res.toString().length) return Promise.resolve() // Already running, resolve
-        return proc.run(cur.args, true)
-          .then(() => services.running.push({ name: curName, stopTimeSecs: cur.stopTimeSecs }))
-          .catch(() => errors.push(cur.name))
-      })
-    }, svc))
-    .then(() => {
+    return Promise.all(
+      _.map((cur) => {
+        let curName = command.getName(cur.name, { persist: cur.persist })
+        return proc.exec(`docker ps -f name=${curName} -q`).then((res) => {
+          if (res && res.toString().length) return Promise.resolve() // Already running, resolve
+          return proc
+            .run(cur.args, true)
+            .then(() =>
+              services.running.push({
+                name: curName,
+                stopTimeSecs: cur.stopTimeSecs
+              })
+            )
+            .catch(() => errors.push(cur.name))
+        })
+      }, svc)
+    ).then(() => {
       const startError = new Error()
       if (errors.length) {
         startError.svcs = errors
@@ -104,23 +133,29 @@ const services = {
     const errors = []
     return Promise.all(
       _.pipe([
-        _.filter(svc => _.test(/bc_/, svc.name)),
-        _.map(svc => proc.run(['stop', '-t', svc.stopTimeSecs, svc.name], true)
-          .then(() => cfg.rmOnShutdown ? proc.run(['rm', svc.name], true) : Promise.resolve())
-          .catch(() => {
-            /* istanbul ignore next: difficult to make a container shutdown fail reliably */
-            errors.push(svc.name)
-          })
+        _.filter((svc) => _.test(/bc_/, svc.name)),
+        _.map((svc) =>
+          proc
+            .run(['stop', '-t', svc.stopTimeSecs, svc.name], true)
+            .then(() =>
+              cfg.rmOnShutdown
+                ? proc.run(['rm', svc.name], true)
+                : Promise.resolve()
+            )
+            .catch(() => {
+              /* istanbul ignore next: difficult to make a container shutdown fail reliably */
+              errors.push(svc.name)
+            })
         )
-      ])(services.running))
-      .then(() => {
-        const stopError = new Error()
-        /* istanbul ignore next: this is actually tested, istanbul... */
-        if (errors.length) {
-          stopError.svcs = errors
-          throw stopError
-        }
-      })
+      ])(services.running)
+    ).then(() => {
+      const stopError = new Error()
+      /* istanbul ignore next: this is actually tested, istanbul... */
+      if (errors.length) {
+        stopError.svcs = errors
+        throw stopError
+      }
+    })
   }
 }
 
